704efff2d81019979ec0b6098b511c624908fac9
Mahmoud Nasser <mnasserone@gmail.com>
2024-02-22 16:54:40 +0200

    rollback saved documents middleware added

---
+++ src/middlewares/rollback-saved-documents.middleware.js
@@ -0,0 +1,27 @@
+import { deleteDocumentByFindByIdAndDelete } from "../../DB/dbMethods";
+
+/**
+ * Middleware to rollback saved documents
+ * 
+ * @param {import('express').Request} req - Express request object. 
+ * @param {import('express').Response} res - Express response object.
+ * @param {import('express').NextFunction} next - Express next function.
+ */
+export const rollbackSavedDocuments = async (req, res, next) => {
+
+    // Log the rollbackSavedDocuments middleware
+    console.log('rollbackSavedDocuments middleware');
+
+    // Che if there are any saved documents in the request object
+    if(req.savedDocuments) {
+
+        // Log the saved documents
+        console.log(req.savedDocuments);
+
+        // Destructure the model and _id from the saved documents
+        const {model, _id} = req.savedDocuments;
+
+        // Delete the document from the database using the 'findByIdAndDelete' method
+        await deleteDocumentByFindByIdAndDelete(model, _id);
+    }
+}
