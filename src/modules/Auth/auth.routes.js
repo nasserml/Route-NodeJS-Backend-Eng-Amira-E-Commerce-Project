ff382b0c33a4b5d088a0750f67d2ee96b7a0423e
Mahmoud Nasser <mnasserone@gmail.com>
2024-02-22 21:10:18 +0200

    sign up schema added to auth routes modules

--- src/modules/Auth/auth.routes.js
+++ src/modules/Auth/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router} from 'express';
 import * as authController from './auth.controller.js';
 import expressAsyncHandler from 'express-async-handler';
+import {validationMiddleware} from '../../middlewares/validation.middleware.js'
+import { signUpSchema } from './auth.validationSchemas.js';
 
 const router = Router();
 
-router.post('/sign-up', expressAsyncHandler(authController.signUpAPI));
+router.post('/sign-up',validationMiddleware(signUpSchema),  expressAsyncHandler(authController.signUpAPI));
 
 router.get('/verfiy-email', expressAsyncHandler(authController.verfiyEmailAPI));
 
