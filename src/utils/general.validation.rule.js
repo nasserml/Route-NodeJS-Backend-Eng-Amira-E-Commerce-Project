a070adaf8d42b5c5bc84f9f098eef23a29d0d6ee
Mahmoud Nasser <mnasserone@gmail.com>
2024-02-22 16:38:05 +0200

    general validation rule utils added

---
+++ src/utils/general.validation.rule.js
@@ -0,0 +1,46 @@
+import Joi from 'joi';
+import { Types } from 'mongoose';
+
+/**
+ * Validate if the given value is a valid ObjectId.
+ * 
+ * @function
+ * @param {string} value - The value to be validated.
+ * @param {object} helper - The Joi validation helper. 
+ * @returns {string} The validated ObjectId or an error message if validation fails.
+ */
+const objectValidation = (value, helper) => {
+
+    // Check if the value is a valid ObjectId
+    const isValid = Types.ObjectId.isValid(value);
+    
+    // Return the validated ObjectId or an error message
+    return (isValid ? value : helper.message('Invalid objectId'));
+};
+
+
+/**
+ * Validation rules for common scenarios used across the application.
+ * 
+ * @constant
+ * @type {Object}
+ * @property {Joi.string} dbId - Joi schema for validation MongoDB ObjectId strings.
+ * @property {Joi.object} headersRules - Joi schema for validation request headers.
+ */
+export const generalRules = {
+    dbId: Joi.string().custom(objectValidation),
+    headersRule: Joi.object({
+        accesstoken: Joi.string().required(),
+        'user-agent': Joi.string().required(),
+        'postman-token': Joi.string(),
+        'content-type': Joi.string(),
+        'accept-encoding': Joi.string(),
+        'cache-control': Joi.string(),
+        'accept': Joi.string(),
+        'host': Joi.string(),
+        'accept-language': Joi.string(),
+        'cookie': Joi.string(),
+        'connection': Joi.string(),
+        'content-length': Joi.string()
+    })
+};
