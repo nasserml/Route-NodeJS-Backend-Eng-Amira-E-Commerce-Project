1c3eacdfee85ee8d07a89771b814ac17a9371d65
Mahmoud Nasser <mnasserone@gmail.com>
2024-02-25 17:24:52 +0200

    validation schema for parmas added to the updata and delete category category validation schema and category route in the category module

--- src/modules/Category/category.routes.js
+++ src/modules/Category/category.routes.js
@@ -6,17 +6,17 @@ import { endPointsRoles } from './category.endpoints.roles.js';
 import {auth} from '../../middlewares/auth.middleware.js';
 import { allowedExtensions } from '../../utils/allowedExtensions.js';
 import { validationMiddleware } from '../../middlewares/validation.middleware.js';
-import { addCategorySchema } from './category.validationSchemas.js';
+import { addCategorySchema, deleteCategorySchema, updateCategorySchema } from './category.validationSchemas.js';
 
 const router = Router();
 
 router.post('/add-category', auth(endPointsRoles.ADD_CATEGORY),  multerMiddleHost({extensions: allowedExtensions.image}).single('image'), expressAsyncHandler(categoryController.addCategoryAPI));
 
-router.put('/update-category/:categoryId', auth(endPointsRoles.UPDATE_CATEGORY), multerMiddleHost({extensions: allowedExtensions.image}).single('image'), expressAsyncHandler(categoryController.updateCategoryAPI));
+router.put('/update-category/:categoryId', validationMiddleware(updateCategorySchema),auth(endPointsRoles.UPDATE_CATEGORY), multerMiddleHost({extensions: allowedExtensions.image}).single('image'), expressAsyncHandler(categoryController.updateCategoryAPI));
 
 router.get('/get-all-categories', expressAsyncHandler(categoryController.getAllCategoriesAPI));
 
-router.delete('/delete-category/:categoryId', auth(endPointsRoles.DELETE_CATEGORY), expressAsyncHandler(categoryController.deleteCategoryAPI))
+router.delete('/delete-category/:categoryId', validationMiddleware(deleteCategorySchema),auth(endPointsRoles.DELETE_CATEGORY), expressAsyncHandler(categoryController.deleteCategoryAPI))
 
 export default router;
 
--- src/modules/Category/category.validationSchemas.js
+++ src/modules/Category/category.validationSchemas.js
@@ -1,9 +1,21 @@
 import Joi from 'joi';
 import { systemRoles } from '../../utils/systemRoles.js';
+import { generalRules } from '../../utils/general.validation.rule.js';
 
 // Schema validation for add category API endpoint
 export const addCategorySchema = {
     body: Joi.object({
         name: Joi.string().required().messages({'any.required': 'Name is required'})
     })
+
+}
+
+// Schema validation for update category API endpoint
+export const updateCategorySchema = {
+    params: Joi.object({categoryId: generalRules.dbId})
+}
+
+// Schema validation for delete category API endpoints
+export const deleteCategorySchema = {
+    params: Joi.object({categoryId: generalRules.Joi})
 }
