4b74391149ef894891fda6de838f9a59c5966f5e
Mahmoud Nasser <mnasserone@gmail.com>
2024-03-02 20:25:51 +0200

    Coupon model added database models

---
+++ DB/models/coupon.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const couponSchema = new mongoose.Schema({
+
+    couponCode: { type: String, required: true, unique: true, trim: true, lowercase: true },
+    couponAmount : { type: Number, required: true, min: 1},
+    couponStatus: { type: String, enum: ['valid', 'expired'], default: 'valid'},
+    isFixed: { type: Boolean},
+    isPrecentage: { type: Boolean},
+    fromDate: { type: String, required: true},
+    toDate : { type: String, required: true},
+    addedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+
+},{ timestamps: true});
+
+export default mongoose.models.Coupon || mongoose.model('Coupon', couponSchema);
+
